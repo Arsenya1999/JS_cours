303514d2466878f657b378e036c4934667de920d
Arsenya1999 <ya.senya2010@mail.ru>
2023-12-25 23:56:42 +0400

    init

---
+++ site/console.js
@@ -0,0 +1,61 @@
+console.log(1)
+console.log(5);
+  
+
+
+
+
+function sum(a, b) {
+    return a+b
+}
+console.log(sum(7,"c"));
+
+
+
+
+
+
+
+
+
+function calculator(a, c, b) {
+    switch(c) {
+        case "+" :
+            return a + b;
+            break;
+        case "-" :
+            return a - b;
+            break;
+        case "*" :
+            return a * b;
+            break;
+        case "/" :
+            return a / b;
+            break;
+        case "%" :
+            return a % b;
+            break;
+        case "**" :
+            return a ** b;
+            break;
+
+    }
+}
+console.log(calculator(5, "%", 3));
+ 
+function year(a){
+    // a=Number
+    // switch(a){
+    //     case"+":
+    //         console.log('Январь');
+    //         break;
+    // }
+
+    if ( a>=0 && a <= 31    ) {
+        return "январь"
+    }
+}
+
+
+console.log(year(23));
+ 
